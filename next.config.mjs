6f7b556be7a435680989f2d77267307a680b470e
UAEHN <120295621+UAEHN@users.noreply.github.com>
2025-04-03 23:52:41 +0400

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // ...existing config
-  output: 'export',
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/shopping-cart' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/shopping-cart/' : '',
-  images: {
-    unoptimized: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
